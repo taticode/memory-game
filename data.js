6ffe6801c5b1a5d5e1e9b4d80719aecd6a670acc
taticode <tatiana.gl.3011@gmail.com>
2026-04-12 21:29:56 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,118 @@
+
+let animales = [
+    { clave: 'gamba', languages: { spanish: 'gamba', english: 'shrimp' }, icono: './assets/img/animales/shrimp.png' },
+    { clave: 'delfin', languages: { spanish: 'delfín', english: 'dolphin' }, icono: './assets/img/animales/dolphin.png' },
+    { clave: 'pez', languages: { spanish: 'pez', english: 'fish' }, icono: './assets/img/animales/fish.png' },
+    { clave: 'rana', languages: { spanish: 'rana', english: 'frog' }, icono: './assets/img/animales/frog.png' },
+    { clave: 'saltamontes', languages: { spanish: 'saltamontes', english: 'grasshopper' }, icono: './assets/img/animales/grasshopper.png' },
+    { clave: 'mariquita', languages: { spanish: 'mariquita', english: 'ladybug' }, icono: './assets/img/animales/ladybug.png' },
+    { clave: 'suricata', languages: { spanish: 'suricata', english: 'meerkat' }, icono: './assets/img/animales/meerkat.png' },
+    { clave: 'pulpo', languages: { spanish: 'pulpo', english: 'octopus' }, icono: './assets/img/animales/octopus.png' },
+    { clave: 'pinguino', languages: { spanish: 'pingüino', english: 'penguin' }, icono: './assets/img/animales/penguin.png' },
+    { clave: 'osopolar', languages: { spanish: 'oso polar', english: 'polar bear' }, icono: './assets/img/animales/polar-bear.png' },
+    { clave: 'conejo', languages: { spanish: 'conejo', english: 'rabbit' }, icono: './assets/img/animales/rabbit.png' },
+    { clave: 'foca', languages: { spanish: 'foca', english: 'seal' }, icono: './assets/img/animales/seal.png' },
+    { clave: 'caracol', languages: { spanish: 'caracol', english: 'snail' }, icono: './assets/img/animales/snail.png' },
+    { clave: 'tortuga', languages: { spanish: 'tortuga', english: 'turtle' }, icono: './assets/img/animales/turtle.png' },
+    { clave: 'ballena', languages: { spanish: 'ballena', english: 'whale' }, icono: './assets/img/animales/whale.png' },
+    { clave: 'cebra', languages: { spanish: 'cebra', english: 'zebra' }, icono: './assets/img/animales/zebra.png' },
+    { clave: 'gato', languages: { spanish: 'gato', english: 'cat' }, icono: './assets/img/animales/cat.png' },
+    { clave: 'camaleon', languages: { spanish: 'camaleón', english: 'chameleon' }, icono: './assets/img/animales/chameleon.png' }
+];
+
+let vegetales = [
+    { clave: 'pimiento', languages: { spanish: 'pimiento', english: 'bell pepper' }, icono: './assets/img/vegetales/bell-pepper.png' },
+    { clave: 'sandia', languages: { spanish: 'sandía', english: 'watermelon' }, icono: './assets/img/vegetales/watermelon.png' },
+    { clave: 'pomelo', languages: { spanish: 'pomelo', english: 'grapefruit' }, icono: './assets/img/vegetales/grapefruit.png' },
+    { clave: 'fresa', languages: { spanish: 'fresa', english: 'strawberry' }, icono: './assets/img/vegetales/strawberry.png' },
+    { clave: 'maiz', languages: { spanish: 'maíz', english: 'corn' }, icono: './assets/img/vegetales/corn.png' },
+    { clave: 'aguacate', languages: { spanish: 'aguacate', english: 'avocado' }, icono: './assets/img/vegetales/avocado.png' },
+    { clave: 'brocoli', languages: { spanish: 'brócoli', english: 'broccoli' }, icono: './assets/img/vegetales/broccoli.png' },
+    { clave: 'zanahoria', languages: { spanish: 'zanahoria', english: 'carrot' }, icono: './assets/img/vegetales/carrot.png' },
+    { clave: 'ajo', languages: { spanish: 'ajo', english: 'garlic' }, icono: './assets/img/vegetales/garlic.png' },
+    { clave: 'setas', languages: { spanish: 'setas', english: 'mushrooms' }, icono: './assets/img/vegetales/mushroom.png' },
+    { clave: 'papaya', languages: { spanish: 'papaya', english: 'papaya' }, icono: './assets/img/vegetales/papaya.png' },
+    { clave: 'espinaca', languages: { spanish: 'espinaca', english: 'spinach' }, icono: './assets/img/vegetales/spinach.png' },
+    { clave: 'tomate', languages: { spanish: 'tomate', english: 'tomato' }, icono: './assets/img/vegetales/tomato.png' },
+    { clave: 'acelga', languages: { spanish: 'acelga', english: 'chard' }, icono: './assets/img/vegetales/chard.png' },
+    { clave: 'rabano', languages: { spanish: 'rábano', english: 'radish' }, icono: './assets/img/vegetales/radish.png' },
+    { clave: 'alcachofa', languages: { spanish: 'alcachofa', english: 'artichoke' }, icono: './assets/img/vegetales/artichoke.png' },
+    { clave: 'berenjena', languages: { spanish: 'berenjena', english: 'eggplant' }, icono: './assets/img/vegetales/eggplant.png' },
+    { clave: 'puerro', languages: { spanish: 'puerro', english: 'leek' }, icono: './assets/img/vegetales/leek.png' }
+];
+
+let comida = [
+    { clave: 'pan', languages: { spanish: 'pan', english: 'bread' }, icono: './assets/img/comida/bread.png' },
+    { clave: 'mermelada', languages: { spanish: 'mermelada', english: 'jam' }, icono: './assets/img/comida/jam.png' },
+    { clave: 'hamburguesa', languages: { spanish: 'hamburguesa', english: 'burger' }, icono: './assets/img/comida/burger.png' },
+    { clave: 'cuscus', languages: { spanish: 'cuscús', english: 'couscous' }, icono: './assets/img/comida/couscous.png' },
+    { clave: 'patatasfritas', languages: { spanish: 'patatas fritas', english: 'french fries' }, icono: './assets/img/comida/french-fries.png' },
+    { clave: 'huevofrito', languages: { spanish: 'huevo frito', english: 'fried egg' }, icono: './assets/img/comida/fried-egg.png' },
+    { clave: 'miel', languages: { spanish: 'miel', english: 'honey' }, icono: './assets/img/comida/honey.png' },
+    { clave: 'perrito', languages: { spanish: 'perrito caliente', english: 'hot-dog' }, icono: './assets/img/comida/hot-dog.png' },
+    { clave: 'te', languages: { spanish: 'té', english: 'tea' }, icono: './assets/img/comida/tea.png' },
+    { clave: 'helado', languages: { spanish: 'helado', english: 'ice cream' }, icono: './assets/img/comida/ice-cream.png' },
+    { clave: 'leche', languages: { spanish: 'leche', english: 'milk' }, icono: './assets/img/comida/milk.png' },
+    { clave: 'tortitas', languages: { spanish: 'tortitas', english: 'pancakes' }, icono: './assets/img/comida/pancake.png' },
+    { clave: 'pizza', languages: { spanish: 'pizza', english: 'pizza' }, icono: './assets/img/comida/pizza.png' },
+    { clave: 'arroz', languages: { spanish: 'arroz', english: 'rice' }, icono: './assets/img/comida/rice.png' },
+    { clave: 'ensalada', languages: { spanish: 'ensalada', english: 'salad' }, icono: './assets/img/comida/salad.png' },
+    { clave: 'brocheta', languages: { spanish: 'brocheta', english: 'skewer' }, icono: './assets/img/comida/skewer.png' },
+    { clave: 'atun', languages: { spanish: 'atún', english: 'tuna' }, icono: './assets/img/comida/tuna.png' },
+    { clave: 'pavo', languages: { spanish: 'pavo asado', english: 'roasted turkey' }, icono: './assets/img/comida/turkey.png' }
+];
+
+let eventos = [
+    { clave: 'globos', languages: { spanish: 'globos', english: 'balloons' }, icono: './assets/img/fiesta/balloons.png' },
+    { clave: 'tarta', languages: { spanish: 'tarta', english: 'cake' }, icono: './assets/img/fiesta/cake.png' },
+    { clave: 'champan', languages: { spanish: 'champán', english: 'champagne' }, icono: './assets/img/fiesta/champagne.png' },
+    { clave: 'invitados', languages: { spanish: 'invitados', english: 'guests' }, icono: './assets/img/fiesta/guests.png' },
+    { clave: 'guirnaldas', languages: { spanish: 'guirnaldas', english: 'garlands' }, icono: './assets/img/fiesta/garlands.png' },
+    { clave: 'regalo', languages: { spanish: 'regalo', english: 'gift' }, icono: './assets/img/fiesta/gift.png' },
+    { clave: 'diadema', languages: { spanish: 'diadema', english: 'headband' }, icono: './assets/img/fiesta/headband.png' },
+    { clave: 'mascara', languages: { spanish: 'máscara', english: 'mask' }, icono: './assets/img/fiesta/mask.png' },
+    { clave: 'microfono', languages: { spanish: 'micrófono', english: 'microphone' }, icono: './assets/img/fiesta/microphone.png' },
+    { clave: 'musica', languages: { spanish: 'música', english: 'music' }, icono: './assets/img/fiesta/music.png' },
+    { clave: 'matasuegras', languages: { spanish: 'matasuegras', english: 'party blower' }, icono: './assets/img/fiesta/party-blower.png' },
+    { clave: 'sombrero', languages: { spanish: 'sombrero', english: 'hat' }, icono: './assets/img/fiesta/party-hat.png' },
+    { clave: 'pinata', languages: { spanish: 'piñata', english: 'pinata' }, icono: './assets/img/fiesta/pinata.png' },
+    { clave: 'popcorn', languages: { spanish: 'palomitas', english: 'popcorn' }, icono: './assets/img/fiesta/popcorn.png' },
+    { clave: 'sandwich', languages: { spanish: 'sandwich', english: 'sandwich' }, icono: './assets/img/fiesta/sandwich.png' },
+    { clave: 'altavoz', languages: { spanish: 'altavoz', english: 'speaker' }, icono: './assets/img/fiesta/speaker.png' },
+    { clave: 'focos', languages: { spanish: 'focos', english: 'spotlights' }, icono: './assets/img/fiesta/spotlight.png' },
+    { clave: 'silbato', languages: { spanish: 'silbato', english: 'whistle' }, icono: './assets/img/fiesta/whistle.png' }
+];
+
+let colegio = [
+    { clave: 'alfabeto', languages: { spanish: 'alfabeto', english: 'alphabet' }, icono: './assets/img/colegio/alphabet.png' },
+    { clave: 'mochila', languages: { spanish: 'mochila', english: 'backpack' }, icono: './assets/img/colegio/backpack.png' },
+    { clave: 'pizarra', languages: { spanish: 'pizarra', english: 'blackboard' }, icono: './assets/img/colegio/blackboard.png' },
+    { clave: 'libros', languages: { spanish: 'libros', english: 'books' }, icono: './assets/img/colegio/books.png' },
+    { clave: 'calculadora', languages: { spanish: 'calculadora', english: 'calculator' }, icono: './assets/img/colegio/calculator.png' },
+    { clave: 'colores', languages: { spanish: 'lápices de color', english: 'color pencils' }, icono: './assets/img/colegio/color-pencils.png' },
+    { clave: 'compas', languages: { spanish: 'compás', english: 'compass' }, icono: './assets/img/colegio/compass.png' },
+    { clave: 'ceras', languages: { spanish: 'ceras', english: 'crayons' }, icono: './assets/img/colegio/crayons.png' },
+    { clave: 'agenda', languages: { spanish: 'agenda', english: 'diary' }, icono: './assets/img/colegio/diary.png' },
+    { clave: 'borrador', languages: { spanish: 'borrador', english: 'eraser' }, icono: './assets/img/colegio/eraser.png' },
+    { clave: 'pegamento', languages: { spanish: 'pegamento', english: 'glue' }, icono: './assets/img/colegio/glue.png' },
+    { clave: 'marcador', languages: { spanish: 'marcador', english: 'marker' }, icono: './assets/img/colegio/marker.png' },
+    { clave: 'boli', languages: { spanish: 'bolígrafo', english: 'pen' }, icono: './assets/img/colegio/pen.png' },
+    { clave: 'lapiz', languages: { spanish: 'lápiz', english: 'pencil' }, icono: './assets/img/colegio/pencil.png' },
+    { clave: 'transpor', languages: { spanish: 'transportador', english: 'protactor' }, icono: './assets/img/colegio/protactor.png' },
+    { clave: 'regla', languages: { spanish: 'regla', english: 'ruler' }, icono: './assets/img/colegio/ruler.png' },
+    { clave: 'bus', languages: { spanish: 'autobús', english: 'bus' }, icono: './assets/img/colegio/school-bus.png' },
+    { clave: 'colegio', languages: { spanish: 'colegio', english: 'school' }, icono: './assets/img/colegio/school.png' },
+    { clave: 'tijeras', languages: { spanish: 'tijeras', english: 'scissors' }, icono: './assets/img/colegio/scissors.png' },
+    { clave: 'sacapuntas', languages: { spanish: 'sacapuntas', english: 'sharpener' }, icono: './assets/img/colegio/sharpener.png' },
+    { clave: 'grapadora', languages: { spanish: 'grapadora', english: 'stapler' }, icono: './assets/img/colegio/stapler.png' },
+    { clave: 'profesora', languages: { spanish: 'profesor/ a', english: 'teacher' }, icono: './assets/img/colegio/teacher.png' }
+];
+
+export const gameState = {
+    animales,
+    vegetales,
+    comida,
+    eventos,
+    colegio,
+    categoriasNames: ['animales', 'vegetales', 'comida', 'eventos', 'colegio']
+};
---
+++ script.js
@@ -0,0 +1,284 @@
+import { gameState } from './data.js';
+
+let flippedCards = [];
+let moves = 0;
+let timer = 0;
+let interval = null;
+let lock = false;
+let matches = 0;
+let currentCategory = 'animales';
+let totalPairs = 0;
+
+let gameHistory = JSON.parse(localStorage.getItem('mem_history')) || {};
+let activeDecks = JSON.parse(localStorage.getItem('mem_decks')) || {};
+let tutorialSeen = localStorage.getItem('mem_tut') === 'true';
+
+const dom = {
+    board: document.getElementById('game-board'),
+    catNav: document.getElementById('cat-nav'),
+    scorePanel: document.getElementById('score-panel'),
+    progressBar: document.getElementById('progress-bar'),
+    pairsCount: document.getElementById('pairs-count'),
+    feedbackText: document.getElementById('feedback-text'),
+    movesDisplay: document.getElementById('moves'),
+    timerDisplay: document.getElementById('timer'),
+    modal: document.getElementById('modal-overlay'),
+    modalBody: document.getElementById('modal-content'),
+    sideMenu: document.getElementById('side-menu'),
+    menuToggle: document.getElementById('menu-toggle'),
+    menuClose: document.getElementById('menu-close'),
+    menuOverlay: document.getElementById('menu-overlay'),
+    resetPanel: document.getElementById('reset-panel-btn'),
+    resetAll: document.getElementById('reset-all-btn'),
+    helpBtn: document.getElementById('help-btn'),
+    shine: document.getElementById('global-shine'),
+    sparkles: document.getElementById('sparkles-container')
+};
+
+/**
+ * Inicializa el estado del panel
+ */
+function init(category) {
+    currentCategory = category;
+    flippedCards = [];
+    lock = false;
+    matches = 0;
+    
+    dom.modal.classList.remove('active');
+    dom.sideMenu.classList.remove('active');
+    
+    clearInterval(interval);
+    interval = null;
+
+    updateMenuData();
+    toggleWinEffects(false); // Desactivar efectos al iniciar o cambiar
+
+    if (gameHistory[category]) {
+        loadCompleted(category);
+    } else {
+        loadNew(category);
+    }
+
+    if (!tutorialSeen) {
+        showPopup('tutorial');
+        tutorialSeen = true;
+        localStorage.setItem('mem_tut', 'true');
+    }
+}
+
+function loadCompleted(cat) {
+    const data = gameHistory[cat];
+    timer = data.timer;
+    moves = data.moves;
+    totalPairs = data.totalPairs;
+    matches = totalPairs;
+    
+    renderStats();
+    dom.progressBar.style.width = '100%';
+    dom.pairsCount.textContent = `${totalPairs}/${totalPairs}`;
+    dom.feedbackText.textContent = "¡Nivel superado!";
+    renderBoard(activeDecks[cat], true);
+    toggleWinEffects(true); // Activar efectos si ya está completado
+}
+
+function loadNew(cat) {
+    timer = 0; moves = 0;
+    renderStats();
+    dom.progressBar.style.width = '0%';
+    
+    let deck = activeDecks[cat];
+    if (!deck) {
+        const pool = [...gameState[cat]].sort(() => Math.random() - 0.5).slice(0, 18);
+        deck = [];
+        pool.forEach(item => {
+            deck.push({ id: item.clave, text: item.languages.english, img: item.icono, type: 'english' });
+            deck.push({ id: item.clave, text: item.languages.spanish, img: item.icono, type: 'spanish' });
+        });
+        deck.sort(() => Math.random() - 0.5);
+        activeDecks[cat] = deck;
+        localStorage.setItem('mem_decks', JSON.stringify(activeDecks));
+    }
+    
+    totalPairs = deck.length / 2;
+    dom.pairsCount.textContent = `0/${totalPairs}`;
+    dom.feedbackText.textContent = "Encuentra parejas";
+    renderBoard(deck, false);
+}
+
+function renderBoard(deck, isDone) {
+    dom.board.innerHTML = '';
+    deck.forEach(data => {
+        const card = document.createElement('div');
+        card.className = `card ${isDone ? 'flipped matched' : ''}`;
+        card.innerHTML = `
+            <div class="card-inner">
+                <div class="card-face card-back"></div>
+                <div class="card-face card-front ${data.type}">
+                    <div class="image-wrapper"><img src="${data.img}" class="icon-img"></div>
+                    <div class="word-text">${data.text}</div>
+                </div>
+            </div>`;
+        if (!isDone) card.onclick = () => handleCardClick(card, data.id);
+        dom.board.appendChild(card);
+    });
+}
+
+function handleCardClick(card, id) {
+    if (lock || card.classList.contains('flipped')) return;
+    if (!interval) startTimer();
+
+    card.classList.add('flipped');
+    flippedCards.push({ card, id });
+
+    if (flippedCards.length === 2) {
+        lock = true;
+        moves++;
+        renderStats();
+        
+        const [c1, c2] = flippedCards;
+        if (c1.id === c2.id) {
+            c1.card.classList.add('matched');
+            c2.card.classList.add('matched');
+            matches++;
+            flippedCards = [];
+            lock = false;
+            dom.progressBar.style.width = `${(matches/totalPairs)*100}%`;
+            dom.pairsCount.textContent = `${matches}/${totalPairs}`;
+            if (matches === totalPairs) handleWin();
+        } else {
+            setTimeout(() => {
+                c1.card.classList.remove('flipped');
+                c2.card.classList.remove('flipped');
+                flippedCards = [];
+                lock = false;
+            }, 800);
+        }
+    }
+}
+
+function handleWin() {
+    clearInterval(interval);
+    gameHistory[currentCategory] = { moves, timer, totalPairs };
+    localStorage.setItem('mem_history', JSON.stringify(gameHistory));
+    
+    toggleWinEffects(true);
+    showPopup('victory');
+    updateMenuData();
+}
+
+/**
+ * Control de efectos visuales de victoria persistentes
+ */
+function toggleWinEffects(active) {
+    if (active) {
+        dom.shine.classList.add('active');
+        createSparkles();
+    } else {
+        dom.shine.classList.remove('active');
+        dom.sparkles.innerHTML = '';
+    }
+}
+
+function createSparkles() {
+    dom.sparkles.innerHTML = '';
+    for (let i = 0; i < 15; i++) {
+        const sparkle = document.createElement('div');
+        sparkle.className = 'sparkle';
+        sparkle.style.top = Math.random() * 100 + '%';
+        sparkle.style.left = Math.random() * 100 + '%';
+        sparkle.style.animationDelay = Math.random() * 2 + 's';
+        dom.sparkles.appendChild(sparkle);
+    }
+}
+
+/**
+ * Modales y Menús
+ */
+function updateMenuData() {
+    dom.catNav.innerHTML = '';
+    const colors = { animales: '#f14343', vegetales: '#22ac0a', comida: '#ff8800', eventos: '#ff00ff', colegio: '#2196f3' };
+    
+    gameState.categoriasNames.forEach(name => {
+        const isComp = !!gameHistory[name];
+        const btn = document.createElement('button');
+        btn.className = `btn-cat ${isComp ? 'completed' : ''}`;
+        btn.style.backgroundColor = colors[name] || '#333';
+        btn.innerHTML = `${name.toUpperCase()} ${isComp ? '✓' : ''}`;
+        if (!isComp) btn.onclick = () => init(name);
+        dom.catNav.appendChild(btn);
+    });
+
+    dom.scorePanel.innerHTML = '';
+    Object.entries(gameHistory).forEach(([cat, data]) => {
+        const div = document.createElement('div');
+        div.className = 'score-entry';
+        div.innerHTML = `<span>${cat.toUpperCase()}</span><span>${formatTime(data.timer)} | ${data.moves} mov.</span>`;
+        dom.scorePanel.appendChild(div);
+    });
+}
+
+function showPopup(type) {
+    dom.modalBody.innerHTML = '';
+    if (type === 'victory') {
+        dom.modalBody.innerHTML = `
+            <h2 style="color:var(--success)">🏆 ¡INCREÍBLE!</h2>
+            <p style="margin: 10px 0;">Has dominado el nivel ${currentCategory}.</p>
+            <button class="btn-action secondary" style="width:100%;margin-top:15px" onclick="location.reload()">SIGUIENTE</button>`;
+    } else if (type === 'tutorial') {
+        dom.modalBody.innerHTML = `
+            <div class="modal-flex">
+                <div class="modal-mascot-side">
+                    <div class="modal-speech">Hello!</div>
+                    <img src="./assets/img/jirafa.png" alt="Jirafa" class="modal-mascot-img">
+                </div>
+                <div class="modal-text-side">
+                    <h3 style="color:var(--info); margin-bottom: 10px;">¿Cómo jugar?</h3>
+                    <p style="font-size:0.75rem;line-height:1.4;">
+                        🧩 <b>Mecánica:</b> Empareja la palabra en inglés con su traducción al español.<br><br>
+                        🔄 <b>Aleatorio:</b> ¡Las 18 parejas cambian cada vez!<br><br>
+                        🚫 <b>Sin scroll:</b> ¡Todo a la vista!
+                    </p>
+                </div>
+            </div>
+            <button class="btn-action secondary" style="width:100%; margin-top: 20px;" onclick="document.getElementById('modal-overlay').classList.remove('active')">¡A JUGAR!</button>`;
+    } else if (type === 'confirmReset') {
+        dom.modalBody.innerHTML = `
+            <h2 style="color:var(--error)">¿RESETEAR TODO?</h2>
+            <p style="margin: 10px 0;">Se borrará tu progreso permanentemente.</p>
+            <div style="display:flex;gap:10px;margin-top:15px">
+                <button class="btn-action danger" id="btn-yes-all">SÍ</button>
+                <button class="btn-action secondary" onclick="document.getElementById('modal-overlay').classList.remove('active')">NO</button>
+            </div>`;
+        document.getElementById('btn-yes-all').onclick = () => { localStorage.clear(); location.reload(); };
+    }
+    dom.modal.classList.add('active');
+}
+
+function startTimer() { interval = setInterval(() => { timer++; renderStats(); }, 1000); }
+function renderStats() { dom.timerDisplay.textContent = formatTime(timer); dom.movesDisplay.textContent = moves; }
+function formatTime(s) { return `${Math.floor(s/60).toString().padStart(2, '0')}:${(s%60).toString().padStart(2, '0')}`; }
+
+// Listeners Acordeón
+document.querySelectorAll('.accordion-header').forEach(header => {
+    header.onclick = () => {
+        const content = document.getElementById(header.dataset.target);
+        header.classList.toggle('active');
+        content.classList.toggle('open');
+    };
+});
+
+dom.menuToggle.onclick = () => dom.sideMenu.classList.add('active');
+dom.menuClose.onclick = () => dom.sideMenu.classList.remove('active');
+dom.menuOverlay.onclick = () => dom.sideMenu.classList.remove('active');
+
+dom.resetPanel.onclick = () => { 
+    delete activeDecks[currentCategory]; delete gameHistory[currentCategory]; 
+    localStorage.setItem('mem_decks', JSON.stringify(activeDecks));
+    localStorage.setItem('mem_history', JSON.stringify(gameHistory));
+    init(currentCategory); 
+};
+
+dom.resetAll.onclick = () => showPopup('confirmReset');
+dom.helpBtn.onclick = () => showPopup('tutorial');
+
+init('animales');
